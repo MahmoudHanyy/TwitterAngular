991ae37bfb4b6edeec13be74b987047af4cea6eb
mahmoudhanyy <mahmoud.hanyy007@gmail.com>
2023-12-27 22:59:08 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: NavbarComponent, },
+    { path: 'home', component: HomeComponent, },
+];
